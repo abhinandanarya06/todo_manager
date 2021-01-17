819a96c24c0e29bba0fc0b1e6a221e600201c4d4
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-01-17 14:56:15 +0530

    Can see /todo/:id

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,10 @@ class TodosController < ApplicationController
   def index
     render plain: Todo.all.map { |todo| todo.formatted_string }.join("\n")
   end
+
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render plain: todo.formatted_string
+  end
 end
