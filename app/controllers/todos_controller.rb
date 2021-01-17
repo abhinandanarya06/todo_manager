5f393016552619e0382b905ca758ac66c98c39d1
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-01-17 16:49:56 +0530

    PATCH to mark as complete is live

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -22,4 +22,13 @@ class TodosController < ApplicationController
     )
     render plain: "New Todo is created with id = #{new_todo.id}"
   end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "Todo : #{todo.todo_text} is marked [#{todo.completed ? "X" : " "}]"
+  end
 end
