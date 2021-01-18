17048695ddd8cdf1fc750dabc246b2f1ed562450
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-01-18 15:20:35 +0530

    format output for /todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 class Todo < ActiveRecord::Base
   def formatted_string
-    "#{id} #{due_date.to_s(:long)} #{todo_text} [#{completed ? "X" : " "}]"
+    "#{id}. #{due_date.to_s(:long)} #{todo_text} [#{completed ? "X" : " "}]"
   end
 end
