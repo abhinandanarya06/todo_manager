1bd6f25e60760d5a4d4ead7760f91ff486ab9c61
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-01-19 21:42:01 +0530

    Ready for live on internet

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,13 @@
 class Todo < ActiveRecord::Base
+  # ------------------ FILTER METHODS -----------------------
+  # 1. overdue
+  # 2. due_today
+  # 3. due_later
+  scope :overdue, -> { where("due_date < ?", Date.today) }
+  scope :due_today, -> { where("due_date = ?", Date.today) }
+  scope :due_later, -> { where("due_date > ?", Date.today) }
+  scope :completed, -> { where(completed: true) }
+
   def formatted_string
     "#{id}. #{due_date.to_s(:long)} #{todo_text} [#{completed ? "X" : " "}]"
   end
