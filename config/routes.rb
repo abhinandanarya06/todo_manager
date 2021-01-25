bfbbfdde09aa08f69f04b8878f0d7361ace7ab48
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-01-25 10:36:49 +0530

    fixed mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get "/", to: "home#index"
   resources "todos"
-  post "/todos/:id", to: "todos#update"
   resources "users"
   get "/signin" => "sessions#new", as: :new_signin
   post "/signin" => "sessions#create", as: :signin
